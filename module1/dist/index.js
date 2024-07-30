d67c6a0b2faefcd4955db440f9dfcb653d81077e
Aditto dev <computerscience01770@gmail.com>
2024-07-31 02:49:21 +1200

    Basic data type

--- module1/dist/index.js
+++ module1/dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
 let course = "next level web development";
-console.log(course);
+// console.log(course);
 const roll = 50;
 const friends = ["Html", "Css", "Js", "React", "Next js", "Ts"];
