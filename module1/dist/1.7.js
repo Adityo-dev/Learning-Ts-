b1c4fe2f96d11c4168b2259c956663f86eee9c2e
Aditto dev <computerscience01770@gmail.com>
2024-08-04 02:44:38 +1200

    intersection type Done

--- module1/dist/1.7.js
+++ module1/dist/1.7.js
@@ -1,25 +1,8 @@
 "use strict";
 {
-    const newDeveloper = "Pro Developer";
-    const user = {
-        name: "a",
-        email: "k@gmail.com",
-        gender: "male",
-        bloodGroup: "B+",
+    const fullstackDeveloper = {
+        skills: ["HTML", "CSS", "JS", "TS", "REACT", "NEXT JS", "EXPRESS"],
+        designation1: "Frontend Developer",
+        designation2: "Backend Developer",
     };
-    // -------intersection typ------
-    //   type FrontendDeveloper = {
-    //     skills: string[];
-    //     designation1: "Frontend Developer";
-    //   };
-    //   type BackendDeveloper = {
-    //     skills: string[];
-    //     designation2: "Backend Developer";
-    //   };
-    //   type FullstackDeveloper = FrontendDeveloper & BackendDeveloper;
-    //   const fullstackDeveloper: FullstackDeveloper = {
-    //     skills: ["HTML", "CSS", "JS", "TS", "REACT", "NEXT JS", "EXPRESS"],
-    //     designation1: "Frontend Developer",
-    //     designation2: "Backend Developer",
-    //   };
 }
--- module1/src/basicDataType/1.7.ts
+++ module1/src/basicDataType/1.7.ts
@@ -21,6 +21,7 @@
     gender: "male",
     bloodGroup: "B+",
   }; */
+
   // -------intersection typ------
 
   type FrontendDeveloper = {
