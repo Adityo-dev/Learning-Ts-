72663ca2511dc1984467ef62583a0d62a22a016c
Aditto dev <computerscience01770@gmail.com>
2024-09-10 13:13:58 +1200

    1.9 ts

--- module1/src/basicDataType/1.8.ts
+++ module1/src/basicDataType/1.8.ts
@@ -5,9 +5,15 @@
 
   const isAdult = age >= 18 ? "Adult" : "Not Adult";
 
+  const myFriendAge: number = 21;
+  const isAdultMan = age >= 18 ? "is Adult Man" : "is Not Adult Man";
+
   //  nullish coalescing operator( null / undefined --> decision making)----------->
   const isAuthenticated = undefined;
   const result = isAuthenticated ?? "Guest";
+
+  const isManAuthenticated = undefined;
+  const manResult = isManAuthenticated ?? "Guest";
 
   //   optional chaining-------->
   type User = {
